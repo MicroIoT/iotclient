ccb57c26caa6420b4d99f9ec6e91b86546dfc5a8
caoxinyu <caoxinyu@gmail.com>
2019-12-02 16:09:58 +0800

    refactoring

--- src/main/java/top/microiot/client/IotclientApplication.java
+++ src/main/java/top/microiot/client/IotclientApplication.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.core.ParameterizedTypeReference;
